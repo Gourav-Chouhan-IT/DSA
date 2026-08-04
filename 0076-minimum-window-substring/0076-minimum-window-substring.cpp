de91440c7a87890da1d75b2d649f225f44a85ae0
Gourav Chouhan <gourav.chouhan.it@gmail.com>
2026-08-04 20:36:09 +0530

    Time: 0 ms (100%), Space: 11.5 MB (65.31%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,50 +1,45 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if (s.empty() || t.empty() || s.length() < t.length()) return "";
+        
+        int count = 0;
+        vector<int> hash(128,0);
+        for( char c : t){
+            if(hash[c] == 0) count++;
+            hash[c]++;
 
-        vector<int> t_freq(128, 0);
-        int required = 0; 
-
-        for (char c : t) {
-            if (t_freq[c] == 0) required++; 
-            t_freq[c]++;
         }
 
-        vector<int> window_freq(128, 0);
-        int formed = 0; 
-
-        int l = 0, r = 0;
+        int l = 0; int r =0;
+        int formed = 0;
+        int start_index = 0;
         int min_len = INT_MAX;
-        int start_idx = 0; 
+        vector<int> window_freq(128,0);
 
-        while (r < s.length()) {
+        while(r < s.length()){
             char c = s[r];
             window_freq[c]++;
 
-            if (t_freq[c] > 0 && window_freq[c] == t_freq[c]) {
+            if(hash[c] > 0 &&  window_freq[c] == hash[c]){
                 formed++;
             }
 
-            while (formed == required) {
+            while(formed == count){
                 if (r - l + 1 < min_len) {
                     min_len = r - l + 1;
-                    start_idx = l;
+                    start_index = l;
                 }
-
+                
                 char left_char = s[l];
                 window_freq[left_char]--;
 
-                if (t_freq[left_char] > 0 && window_freq[left_char] < t_freq[left_char]) {
+                if (hash[left_char] > 0 && window_freq[left_char] < hash[left_char]) {
                     formed--;
                 }
-
-                l++; 
+                l++;
             }
-
-            r++; 
+            r++;
         }
-
-        return min_len == INT_MAX ? "" : s.substr(start_idx, min_len);
+        return min_len == INT_MAX ? "" : s.substr(start_index, min_len);
     }
 };
