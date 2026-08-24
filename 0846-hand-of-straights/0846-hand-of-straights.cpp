45075d756d39f4adf632fcf69f21eee881e633dd
Gourav Chouhan <gourav.chouhan.it@gmail.com>
2026-08-24 12:26:27 +0530

    Time: 41 ms (42.78%), Space: 32.4 MB (43%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <map>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        if (hand.size() % groupSize != 0) {
+            return false;
+        }
+
+        map<int, int> count;
+        for (int card : hand) {
+            count[card]++;
+        }
+
+        for (auto it = count.begin(); it != count.end(); ++it) {
+            int card = it->first;
+            int freq = it->second;
+
+            if (freq > 0) {
+                for (int i = 1; i < groupSize; ++i) {
+                    int nextCard = card + i;
+                    if (count[nextCard] < freq) {
+                        return false;
+                    }
+                    count[nextCard] -= freq;
+                }
+            }
+        }
+
+        return true;
+    }
+};
