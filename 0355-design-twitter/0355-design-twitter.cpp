218eef91a1aac00bc57643a6d72a9f78bc17f619
Gourav Chouhan <gourav.chouhan.it@gmail.com>
2026-08-24 16:33:12 +0530

    Time: 12 ms (32.3%), Space: 55.8 MB (85.96%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.cpp
+++ 0355-design-twitter/0355-design-twitter.cpp
@@ -1,83 +1,64 @@
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <tuple>
+
+using namespace std;
+
 class Twitter {
 public:
     unordered_map<int, unordered_set<int>> following;
     unordered_map<int, vector<pair<int,int>>> tweets;
     int timestamp = 0;
 
-    Twitter() {
-       
-    }
-    
+    Twitter() {}
+
     void postTweet(int userId, int tweetId) {
         timestamp++;
         tweets[userId].push_back({timestamp, tweetId});
     }
-    
+
     vector<int> getNewsFeed(int userId) {
-        
         vector<int> result;
+        vector<tuple<int, int, int>> initialCandidates;
 
-        // Max heap:
-        // {timestamp, userId, index}
-        priority_queue<tuple<int, int, int>> pq;
-
-        // User's own tweets must also appear
+        // 1. Collect most recent tweet for self and followees: O(F)
         if (!tweets[userId].empty()) {
             int index = tweets[userId].size() - 1;
-
-            pq.push({
-                tweets[userId][index].first,
-                userId,
-                index
-            });
+            initialCandidates.push_back({tweets[userId][index].first, userId, index});
         }
 
-        // Add the newest tweet of every person userId follows
         for (int followeeId : following[userId]) {
-
             if (!tweets[followeeId].empty()) {
-
                 int index = tweets[followeeId].size() - 1;
-
-                pq.push({
-                    tweets[followeeId][index].first,
-                    followeeId,
-                    index
-                });
+                initialCandidates.push_back({tweets[followeeId][index].first, followeeId, index});
             }
         }
 
-        // Get at most 10 newest tweets
-        while (!pq.empty() && result.size() < 10) {
+        // 2. Linear-time Heapify: O(F) via iterator range constructor
+        priority_queue<tuple<int, int, int>> pq(initialCandidates.begin(), initialCandidates.end());
 
+        // 3. Extract top K (K=10) tweets: O(K log F)
+        while (!pq.empty() && result.size() < 10) {
             auto [time, currentUser, index] = pq.top();
             pq.pop();
 
-            // Add tweet ID to result
-            result.push_back(
-                tweets[currentUser][index].second
-            );
+            result.push_back(tweets[currentUser][index].second);
 
-            // Move to the next older tweet of same user
             if (index > 0) {
-
                 index--;
-
-                pq.push({
-                    tweets[currentUser][index].first,
-                    currentUser,
-                    index
-                });
+                pq.push({tweets[currentUser][index].first, currentUser, index});
             }
         }
 
         return result;
     }
-    
+
     void follow(int followerId, int followeeId) {
         following[followerId].insert(followeeId);
     }
-    
+
     void unfollow(int followerId, int followeeId) {
         following[followerId].erase(followeeId);
     }
