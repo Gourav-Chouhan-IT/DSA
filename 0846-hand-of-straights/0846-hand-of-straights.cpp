c24a7e08f3e8658cebbc3ad5a22f94d1e5e380a4
Gourav Chouhan <gourav.chouhan.it@gmail.com>
2026-08-24 12:27:15 +0530

    Time: 34 ms (73.9%), Space: 31 MB (87.14%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -1,32 +1,40 @@
 #include <vector>
-#include <map>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
-        if (hand.size() % groupSize != 0) {
-            return false;
-        }
+        if (hand.size() % groupSize != 0) return false;
+        if (groupSize == 1) return true;
 
-        map<int, int> count;
+        unordered_map<int, int> count;
         for (int card : hand) {
             count[card]++;
         }
 
-        for (auto it = count.begin(); it != count.end(); ++it) {
-            int card = it->first;
-            int freq = it->second;
+        for (int card : hand) {
+            if (count[card] == 0) continue;
 
-            if (freq > 0) {
-                for (int i = 1; i < groupSize; ++i) {
-                    int nextCard = card + i;
-                    if (count[nextCard] < freq) {
-                        return false;
+            // Walk backward to find the start of this contiguous sequence
+            int start = card;
+            while (count.count(start - 1) && count[start - 1] > 0) {
+                start--;
+            }
+
+            // Process groups sequentially starting from 'start'
+            while (start <= card) {
+                int freq = count[start];
+                if (freq > 0) {
+                    for (int i = 0; i < groupSize; ++i) {
+                        if (count[start + i] < freq) {
+                            return false;
+                        }
+                        count[start + i] -= freq;
                     }
-                    count[nextCard] -= freq;
                 }
+                start++;
             }
         }
 
