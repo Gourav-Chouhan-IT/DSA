5211c78ccd5187eb1e67a5a17698781408756e09
Gourav Chouhan <gourav.chouhan.it@gmail.com>
2026-08-29 17:26:14 +0530

    Time: 0 ms (100%), Space: 52.4 MB (57.64%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -5,11 +5,11 @@ public:
         int maxReach = 0;
         for (int i = 0; i < n; i++) {
             if (i > maxReach) {
-                return false;   // this index is unreachable
+                return false;   
             }
             maxReach = max(maxReach, i + nums[i]);
             if (maxReach >= n - 1) {
-                return true;    // already proven we can reach the end
+                return true;   
             }
         }
         return true;
